98ac9395f3b715beb7811cf55d66dbb1d78794e3
Amranabdi1 <amranabdikadir101@gmail.c0m>
2022-08-29 12:30:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+function superbowlWin(record) {
+	let obj = record.find(function (obj) {
+		return obj.result === "W";
+	});
+	if (typeof obj === "undefined") {
+		return undefined;
+	} else {
+		return obj.year;
+	}
+}
+function findMatching(drivers, name) {
+  return drivers
+    .filter(function (driver) {
+      return driver.toLowerCase().indexOf(name.toLowerCase()) !== -1;
+    })
+    .sort();
+}
+function fuzzyMatch(drivers, name) {
+  return drivers
+    .filter(function (driver) {
+      return driver.toLowerCase().indexOf(name.toLowerCase()) === 0;
+    })
+    .sort();
+}
+function matchName(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
+
+
+
